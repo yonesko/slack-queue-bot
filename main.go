fdb90d49ef1a60442376fc886dac624d2d7adabc
Gleb <g.danichev@city-mobil.ru>
2020-01-15 23:24:30 +0300

    main.go

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"slack-queue-bot/queue"
+	"strings"
 
 	"github.com/nlopes/slack"
 )
@@ -15,16 +17,25 @@ func main() {
 		slack.OptionLog(log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)),
 	)
 
-	queueManager := queue.NewService()
-
+	queueService := queue.NewService()
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
 	for msg := range rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
-			rtm.SendMessage(rtm.NewOutgoingMessage(ev.Text, ev.Channel))
-			queueManager.Add(queue.User{Id: "4545"})
+			switch {
+			case strings.HasPrefix(ev.Text, "add"):
+				queueService.Add(queue.User{Id: ev.User})
+				rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprint(queueService.Show()), ev.Channel))
+			case strings.HasPrefix(ev.Text, "del"):
+				queueService.Delete(queue.User{Id: ev.User})
+				rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprint(queueService.Show()), ev.Channel))
+			case strings.HasPrefix(ev.Text, "show"):
+				rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprint(queueService.Show()), ev.Channel))
+			}
+		case *slack.OutgoingErrorEvent:
+			fmt.Printf("Can't send msg: %s", ev.Error())
 		}
 	}
 }
