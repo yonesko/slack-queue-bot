2badf93c9e39abf4bab14c2f02acd278bf061af8
Глеб Даничев <g.danichev@city-mobil.ru>
2020-01-16 13:20:47 +0300

    TestService_Add_Idempotent

--- queue/service_test.go
+++ queue/service_test.go
@@ -14,3 +14,15 @@ func TestService_Add_1(t *testing.T) {
 	}
 	equals(queue, []string{"123"})
 }
+
+func TestService_Add_Idempotent(t *testing.T) {
+	service := newInmemService()
+	err := service.Add(User{Id: "123"})
+	if err != nil {
+		t.Error(err)
+	}
+	err = service.Add(User{Id: "123"})
+	if err == nil || err.Error() != "already exist" {
+		t.Error("must be already exist")
+	}
+}
