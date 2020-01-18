343eb3db5a4932e88767d26360a2861f5cb05d9d
Gleb <g.danichev@city-mobil.ru>
2020-01-18 23:57:56 +0300

    TestFileRepositoryParallel

--- queue/repository_test.go
+++ queue/repository_test.go
@@ -1,6 +1,19 @@
 package queue
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+	"testing"
+)
+
+func init() {
+	err := os.RemoveAll("db")
+	if err != nil {
+		panic(err)
+	}
+}
 
 func TestFileRepository(t *testing.T) {
 	repository := newFileRepository()
@@ -22,6 +35,36 @@ func TestFileRepository(t *testing.T) {
 		t.Error(err)
 	}
 	assertState(t, queue, []string{"54", "987654"})
+}
+
+func TestFileRepositoryParallel(t *testing.T) {
+	service := NewService()
+	group := &sync.WaitGroup{}
+	group.Add(1)
+
+	addUsers(service, t, 0, 1000, group)
+	group.Wait()
+
+	queue, err := service.Show()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(queue.Users) != 1000 {
+		t.Error("must be all")
+	}
+	for i := 0; i < 1000; i++ {
+		queue.Users[i].Id = strconv.Itoa(i)
+	}
+}
+
+func addUsers(service Service, t *testing.T, start, end int, group *sync.WaitGroup) {
+	for i := start; i < end; i++ {
+		err := service.Add(User{Id: fmt.Sprint(i)})
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	group.Done()
 }
 
 func assertState(t *testing.T, queue Queue, userIds []string) {
