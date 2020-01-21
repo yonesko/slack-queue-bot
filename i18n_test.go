3ec2b0f9b495db2c0483006d26be91a08ad68b0a
Глеб Даничев <g.danichev@city-mobil.ru>
2020-01-21 14:13:27 +0300

    TestAllLabelsAreSet

---
+++ i18n_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yonesko/slack-queue-bot/i18n"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestAllLabelsAreSet(t *testing.T) {
+	usedLabels := collectUsedLabels()
+	if len(usedLabels) == 0 {
+		t.Error("no labels are used, use some or skip this test")
+	}
+	for l, _ := range usedLabels {
+		if val, ok := i18n.P.Get(l); !ok || len(strings.TrimSpace(val)) == 0 {
+			t.Errorf("label %s is undefined", l)
+		}
+	}
+	for _, l := range i18n.P.Keys() {
+		if _, ok := usedLabels[l]; !ok {
+			fmt.Printf("label %s is unused\n", l)
+		}
+	}
+}
+
+func collectUsedLabels() map[string]struct{} {
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, "controller.go", nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+	usedLabels := map[string]struct{}{}
+	if containsI18nPackage(node) {
+		ast.Inspect(node, func(n ast.Node) bool {
+			callExpr, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+			fun, ok := callExpr.Fun.(*ast.SelectorExpr)
+			if !ok {
+				return true
+			}
+			if fun.Sel.Name == "MustGetString" {
+				val, err := strconv.Unquote(callExpr.Args[0].(*ast.BasicLit).Value)
+				if err != nil {
+					panic(fmt.Errorf("strconv.Unquote %s", err))
+				}
+				usedLabels[val] = struct{}{}
+			}
+			return true
+		})
+	}
+	return usedLabels
+}
+
+func containsI18nPackage(f *ast.File) bool {
+	for _, imp := range f.Imports {
+		if imp.Path.Value == "\"github.com/yonesko/slack-queue-bot/i18n\"" {
+			return true
+		}
+	}
+	return false
+
+}
