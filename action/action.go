9f3e50e0f98b5d772753d260d0e6fb05acc67287
Gleb <g.danichev@city-mobil.ru>
2020-01-21 23:05:36 +0300

    AddToQueue

--- action/action.go
+++ action/action.go
@@ -1,5 +1,38 @@
 package action
 
+import (
+	"errors"
+	"github.com/yonesko/slack-queue-bot/model"
+	"github.com/yonesko/slack-queue-bot/queue"
+)
+
+var (
+	AlreadyExistErr = errors.New("already exist")
+	NoSuchUserErr   = errors.New("no such user")
+)
+
 type AddToQueue interface {
-	Do(userId string) error
+	Do(entity model.QueueEntity) error
+}
+
+type addToQueue struct {
+	queueRepository queue.Repository
+}
+
+func (a *addToQueue) Do(entity model.QueueEntity) error {
+	queue, err := a.queueRepository.Read()
+	if err != nil {
+		return err
+	}
+
+	i := queue.IndexOf(entity)
+	if i != -1 {
+		return AlreadyExistErr
+	}
+	queue.Entities = append(queue.Entities, entity)
+	err = a.queueRepository.Save(queue)
+	if err != nil {
+		return err
+	}
+	return nil
 }
