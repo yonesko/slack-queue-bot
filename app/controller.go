51442fd9308d3ab6ffcc996b5f4af19c53afd066
Gleb <g.danichev@city-mobil.ru>
2020-02-04 22:21:40 +0300

    rename receiver

--- app/controller.go
+++ app/controller.go
@@ -30,10 +30,10 @@ func newController(lumberWriter io.Writer, userRepository user.Repository, queue
 	}
 }
 
-func (cont *Controller) execute(command usecase.Command) string {
+func (c *Controller) execute(command usecase.Command) string {
 	defer func() {
 		if r := recover(); r != nil {
-			cont.logger.Printf("catch panic: %#v", r)
+			c.logger.Printf("catch panic: %#v", r)
 			debug.PrintStack()
 		}
 	}()
@@ -42,84 +42,84 @@ func (cont *Controller) execute(command usecase.Command) string {
 	var err error
 	switch command.Data.(type) {
 	case usecase.AddCommand:
-		txt, err = cont.addUser(command.AuthorUserId)
+		txt, err = c.addUser(command.AuthorUserId)
 	case usecase.DelCommand:
-		txt, err = cont.deleteUser(command.AuthorUserId)
+		txt, err = c.deleteUser(command.AuthorUserId)
 	case usecase.ShowCommand:
-		txt, err = cont.showQueue(command.AuthorUserId)
+		txt, err = c.showQueue(command.AuthorUserId)
 	case usecase.CleanCommand:
-		txt, err = cont.clean(command.AuthorUserId)
+		txt, err = c.clean(command.AuthorUserId)
 	case usecase.PopCommand:
-		txt, err = cont.pop(command.AuthorUserId)
+		txt, err = c.pop(command.AuthorUserId)
 	default:
-		cont.logger.Printf("undefined command : %v", command)
-		return cont.showHelp(command.AuthorUserId)
+		c.logger.Printf("undefined command : %v", command)
+		return c.showHelp(command.AuthorUserId)
 	}
 	if err != nil {
-		cont.logger.Println(err)
+		c.logger.Println(err)
 		return i18n.P.MustGetString("error_occurred")
 	}
 	return txt
 }
 
-func (cont *Controller) addUser(authorUserId string) (string, error) {
-	err := cont.queueService.Add(model.QueueEntity{UserId: authorUserId})
+func (c *Controller) addUser(authorUserId string) (string, error) {
+	err := c.queueService.Add(model.QueueEntity{UserId: authorUserId})
 	if err == usecase.AlreadyExistErr {
-		return cont.appendQueue(i18n.P.MustGetString("you_are_already_in_the_queue"), authorUserId), nil
+		return c.appendQueue(i18n.P.MustGetString("you_are_already_in_the_queue"), authorUserId), nil
 	}
 	if err != nil {
 		return "", err
 	}
-	return cont.appendQueue(i18n.P.MustGetString("added_successfully"), authorUserId), nil
+	return c.appendQueue(i18n.P.MustGetString("added_successfully"), authorUserId), nil
 }
 
-func (cont *Controller) deleteUser(authorUserId string) (string, error) {
-	err := cont.queueService.DeleteById(authorUserId, authorUserId)
+func (c *Controller) deleteUser(authorUserId string) (string, error) {
+	err := c.queueService.DeleteById(authorUserId, authorUserId)
 	if err == usecase.NoSuchUserErr {
-		return cont.appendQueue(i18n.P.MustGetString("you_are_not_in_the_queue"), authorUserId), nil
+		return c.appendQueue(i18n.P.MustGetString("you_are_not_in_the_queue"), authorUserId), nil
 	}
 	if err == usecase.QueueIsEmpty {
-		return cont.showQueue(authorUserId)
+		return c.showQueue(authorUserId)
 	}
 	if err != nil {
 		return "", err
 	}
-	return cont.appendQueue(i18n.P.MustGetString("deleted_successfully"), authorUserId), nil
+	return c.appendQueue(i18n.P.MustGetString("deleted_successfully"), authorUserId), nil
 }
 
-func (cont *Controller) appendQueue(txt string, authorUserId string) string {
-	queueTxt, err := cont.showQueue(authorUserId)
+func (c *Controller) appendQueue(txt string, authorUserId string) string {
+	queueTxt, err := c.showQueue(authorUserId)
 	if err != nil {
 		return txt
 	}
 	return txt + "\n" + queueTxt
 }
 
-func (cont *Controller) showQueue(authorUserId string) (string, error) {
-	q, err := cont.queueService.Show()
+func (c *Controller) showQueue(authorUserId string) (string, error) {
+	q, err := c.queueService.Show()
 	if err != nil {
 		return "", err
 	}
-	text, err := cont.composeShowQueueText(q, authorUserId)
+	text, err := c.composeShowQueueText(q, authorUserId)
 	if err != nil {
 		return "", err
 	}
 	return text, nil
 }
 
-func (cont *Controller) composeShowQueueText(queue model.Queue, authorUserId string) (string, error) {
+func (c *Controller) composeShowQueueText(queue model.Queue, authorUserId string) (string, error) {
 	if len(queue.Entities) == 0 {
 		return i18n.P.MustGetString("queue_is_empty"), nil
 	}
 	txt := ""
 	for i, u := range queue.Entities {
-		user, err := cont.userRepository.FindById(u.UserId)
+		user, err := c.userRepository.FindById(u.UserId)
 		if err != nil {
 			return "", fmt.Errorf("can't composeShowQueueText: %s", err)
 		}
 		highlight, holdTime := "", ""
 		if u.UserId == authorUserId {
-			highlight = ":point_left::skin-tone-2:" + cont.estimateTxt(i, queue)
+			highlight = ":point_left::skin-tone-2:" + c.estimateTxt(i, queue)
 		}
 		if i == 0 && queue.HoldTs.Unix() > 0 {
 			holdTime = ":lock: " + time.Now().Sub(queue.HoldTs).Round(time.Minute).String()
@@ -129,52 +129,52 @@ func (cont *Controller) composeShowQueueText(queue model.Queue, authorUserId str
 	return txt, nil
 }
 
-func (cont *Controller) estimateTxt(i int, queue model.Queue) string {
-	estimate, err := cont.estimateRepository.Get()
+func (c *Controller) estimateTxt(i int, queue model.Queue) string {
+	estimate, err := c.estimateRepository.Get()
 	if err != nil {
-		cont.logger.Printf("composeShowQueueText can't get estimate %s", err)
+		c.logger.Printf("composeShowQueueText can't get estimate %s", err)
 		return ""
 	}
 	duration := estimate.TimeToWait(uint(i), queue.HoldTs).Round(time.Minute)
 	return fmt.Sprintf("~%s (%s)", duration, time.Now().Add(duration).Format("Mon Jan 2 15:04:05"))
 }
 
-func (cont *Controller) showHelp(authorUserId string) string {
-	return fmt.Sprintf(i18n.P.MustGetString("help_text"), cont.title(authorUserId))
+func (c *Controller) showHelp(authorUserId string) string {
+	return fmt.Sprintf(i18n.P.MustGetString("help_text"), c.title(authorUserId))
 }
 
-func (cont *Controller) clean(authorUserId string) (string, error) {
-	err := cont.queueService.DeleteAll()
+func (c *Controller) clean(authorUserId string) (string, error) {
+	err := c.queueService.DeleteAll()
 	if err == usecase.QueueIsEmpty {
-		return cont.showQueue(authorUserId)
+		return c.showQueue(authorUserId)
 	}
 	if err != nil {
 		return "", err
 	}
-	return cont.appendQueue(i18n.P.MustGetString("cleaned_successfully"), authorUserId), nil
+	return c.appendQueue(i18n.P.MustGetString("cleaned_successfully"), authorUserId), nil
 }
 
-func (cont *Controller) pop(authorUserId string) (string, error) {
-	deletedUserId, err := cont.queueService.Pop(authorUserId)
+func (c *Controller) pop(authorUserId string) (string, error) {
+	deletedUserId, err := c.queueService.Pop(authorUserId)
 	if err == usecase.QueueIsEmpty {
-		return cont.showQueue(authorUserId)
+		return c.showQueue(authorUserId)
 	}
 	if err != nil {
 		return "", err
 	}
-	txt := fmt.Sprintf(i18n.P.MustGetString("popped_successfully"), cont.deletedUserTxt(deletedUserId))
-	return cont.appendQueue(txt, authorUserId), nil
+	txt := fmt.Sprintf(i18n.P.MustGetString("popped_successfully"), c.deletedUserTxt(deletedUserId))
+	return c.appendQueue(txt, authorUserId), nil
 }
 
-func (cont *Controller) deletedUserTxt(deletedUserId string) string {
-	if user, err := cont.userRepository.FindById(deletedUserId); err == nil {
+func (c *Controller) deletedUserTxt(deletedUserId string) string {
+	if user, err := c.userRepository.FindById(deletedUserId); err == nil {
 		return user.FullName
 	}
 	return ""
 }
 
-func (cont *Controller) title(userId string) string {
-	if user, err := cont.userRepository.FindById(userId); err == nil {
+func (c *Controller) title(userId string) string {
+	if user, err := c.userRepository.FindById(userId); err == nil {
 		return strings.TrimSpace(user.FullName)
 	}
 	return "human"
