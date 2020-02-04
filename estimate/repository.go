176ae2a544a4dbc63a4d7ce45fd0b4b27a4d0b55
Gleb <g.danichev@city-mobil.ru>
2020-02-04 22:26:47 +0300

    avgRestOfHolding

--- estimate/repository.go
+++ estimate/repository.go
@@ -21,14 +21,18 @@ func (e Estimate) AddOne(duration time.Duration) Estimate {
 }
 
 func (e Estimate) TimeToWait(before uint, holdStart time.Time) time.Duration {
-	if before == 0 {
+	if before <= 0 {
 		return 0
 	}
+	return time.Duration(int64(before-1)*e.Average.Nanoseconds()) + e.avgRestOfHolding(holdStart)
+}
+
+func (e Estimate) avgRestOfHolding(holdStart time.Time) time.Duration {
 	holdRest := e.Average - time.Now().Sub(holdStart)
 	if holdRest.Nanoseconds() < 0 {
-		holdRest = 0
+		return 0
 	}
-	return time.Duration(int64(before-1)*e.Average.Nanoseconds()) + holdRest
+	return holdRest
 }
 
 type Repository interface {
