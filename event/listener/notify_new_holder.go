c285a6a4fb8144dc6f7444781c3e81b5c5d8a4ff
Глеб Даничев <g.danichev@city-mobil.ru>
2020-02-05 19:16:10 +0300

    passSleepingHolder

--- event/listener/notify_new_holder.go
+++ event/listener/notify_new_holder.go
@@ -19,23 +19,34 @@ type NotifyNewHolderEventListener struct {
 	queueService usecase.QueueService
 }
 
-const waitAckDur = time.Minute * 7
+const waitForAck = time.Minute * 7
 
 func NewNotifyNewHolderEventListener(slackApi *slack.Client) *NotifyNewHolderEventListener {
 	return &NotifyNewHolderEventListener{slackApi: slackApi}
 }
 
 func (n *NotifyNewHolderEventListener) Fire(newHolderEvent model.NewHolderEvent) {
-	n.sendMsg(newHolderEvent.CurrentHolderUserId, fmt.Sprintf(i18n.P.MustGetString("your_turn_came"), waitAckDur))
+	n.sendMsg(newHolderEvent.CurrentHolderUserId, fmt.Sprintf(i18n.P.MustGetString("your_turn_came"), waitForAck))
+
+	time.AfterFunc(waitForAck, func() {
+		n.passSleepingHolder(newHolderEvent.CurrentHolderUserId)
+	})
 }
 
 func (n *NotifyNewHolderEventListener) passSleepingHolder(holderUserId string) {
 	err := n.queueService.Pass(holderUserId)
+	if err == usecase.YouAreNotHolder {
+		return
+	}
+	if err == usecase.NoOneToPass {
+		n.sendMsg(holderUserId, "я бы передал твой ход следующему, пока ты спишь, то ты один в очереди")
+		return
+	}
 	if err != nil {
 		log.Printf("can't passSleepingHolder %s", err)
 		return
 	}
-	n.sendMsg(holderUserId, "тебя выкинули пока ты спал, hasta la vista")
+	n.sendMsg(holderUserId, "твой ход передался следующему, пока ты спал")
 }
 
 func (n *NotifyNewHolderEventListener) sendMsg(holderUserId, txt string) {
