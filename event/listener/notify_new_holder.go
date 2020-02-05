4961b5d35a588ed1138a42d1abf1a5b52116dab1
Глеб Даничев <g.danichev@city-mobil.ru>
2020-02-05 18:05:03 +0300

    txt := fmt.Sprintf(i18n.P.MustGetString("your_turn_came"), waitAckDur)

--- event/listener/notify_new_holder.go
+++ event/listener/notify_new_holder.go
@@ -1,10 +1,12 @@
 package listener
 
 import (
+	"fmt"
 	"github.com/nlopes/slack"
 	"github.com/yonesko/slack-queue-bot/i18n"
 	"github.com/yonesko/slack-queue-bot/model"
 	"log"
+	"time"
 )
 
 type NewHolderEventListener interface {
@@ -15,6 +17,8 @@ type NotifyNewHolderEventListener struct {
 	slackApi *slack.Client
 }
 
+const waitAckDur = time.Minute * 7
+
 func NewNotifyNewHolderEventListener(slackApi *slack.Client) *NotifyNewHolderEventListener {
 	return &NotifyNewHolderEventListener{slackApi: slackApi}
 }
@@ -23,11 +27,14 @@ func (n *NotifyNewHolderEventListener) Fire(newHolderEvent model.NewHolderEvent)
 	if newHolderEvent.CurrentHolderUserId == "" {
 		return
 	}
+	txt := fmt.Sprintf(i18n.P.MustGetString("your_turn_came"), waitAckDur)
 	_, _, err := n.slackApi.PostMessage(newHolderEvent.CurrentHolderUserId,
-		slack.MsgOptionText(i18n.P.MustGetString("your_turn_came"), true),
+		slack.MsgOptionText(txt, true),
 		slack.MsgOptionAsUser(true),
 	)
 	if err != nil {
 		log.Printf("can't notify %s", err)
+		return
 	}
+
 }
