257178e071a7427c6365c02e00d64bcdf7c1fa82
Глеб Даничев <g.danichev@city-mobil.ru>
2020-02-05 12:44:42 +0300

    1.5.1 Rm seconds in estimate

--- app/app.go
+++ app/app.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	thisBotUserId = "<@USMRFHHPE>" //test bot user USG0TPHGA
-	version       = "1.5.0"
+	version       = "1.5.1"
 )
 
 type App struct {
--- app/controller.go
+++ app/controller.go
@@ -139,7 +139,7 @@ func (c *Controller) estimateTxt(i int, queue model.Queue) string {
 	if duration == 0 {
 		return ""
 	}
-	return fmt.Sprintf("~%s (%s)", duration, time.Now().Add(duration).Format("Mon Jan 2 15:04:05"))
+	return fmt.Sprintf("~%s (%s)", duration, time.Now().Add(duration).Format("Mon Jan 2 15:04"))
 }
 
 func (c *Controller) showHelp(authorUserId string) string {
