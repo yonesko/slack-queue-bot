b090d526fb6018f0d5ef22ab2eed0c24532f3670
Глеб Даничев <g.danichev@city-mobil.ru>
2020-02-11 11:08:23 +0300

    log.Printf("sending to %s '%s'", userId, txt)

--- gateway/mock.go
+++ gateway/mock.go
@@ -1,12 +1,15 @@
 package gateway
 
+import "log"
+
 type Mock struct {
 }
 
 func (m Mock) Send(userId, txt string) error {
+	log.Printf("sending to %s '%s'", userId, txt)
 	return nil
 }
 
 func (m Mock) SendAndLog(userId, txt string) {
-	return
+	_ = m.Send(userId, txt)
 }
